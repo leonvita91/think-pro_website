cfcd5562d1c74b3c3ec6b923ad0b8e2ea202ce90
leonvita91 <leonvita91@gmail.com>
2023-03-12 22:55:03 +0000

    add dark-mode

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 
 
 
+function dark () {
+    let dark_mode = document.body;
+    dark_mode.style.backgroundColor = '#1d2a35';
+    let dark_nav = document.getElementById('nav');
+    dark_nav.style.backgroundColor = '#0d1721';
+    let dark_text = document.getElementById('site-name'); 
+    dark_text.style.color = 'white';
+    let dark_btn = document.getElementById('dark-btn');
+    dark_nav.style.backgroundColor = '#335980';
+}
+
+/*
 function fun () {
     var img = document.getElementById('pic');
     img.src = 'bg1.jpg';
 }
+*/
