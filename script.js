7913e861d58f4cc04b98f2ec0b26c625337e546b
leonvita91 <leonvita91@gmail.com>
2023-03-13 14:53:08 +0000

    add logo on left page

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 
-
+localStorage.setItem('color','#1d2a35');
 
 function dark () {
+    let colour = localStorage.getItem('color');  
     let dark_mode = document.body;
-    dark_mode.style.backgroundColor = '#1d2a35';
+    dark_mode.style.backgroundColor = colour ;
     let dark_nav = document.getElementById('nav');
     dark_nav.style.backgroundColor = '#0d1721';
     let dark_text = document.getElementById('site-name'); 
@@ -18,3 +19,4 @@ function fun () {
     img.src = 'bg1.jpg';
 }
 */
+
