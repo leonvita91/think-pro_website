492b29b7ab0f831b1d501d120d74c5870d20dff6
leonvita91 <leonvita91@gmail.com>
2023-03-22 02:13:13 +0000

    add bg-light to dm-btn

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 
+
 // read the key value from localstorge
 let darkMode = localStorage.getItem('DarkMode');
 
 const DarkMode_toggle = document.querySelector('#dark-mode');
 
-const enableDarkMode = () => {
 
-    document.querySelector('#dark-mode').src="brightness.png";
+
+const enableDarkMode = () => {
+    
+    document.querySelector('#dark-mode').src="light.png";
+
+    
     // add dark-mode when it enable
     let dark_mode = document.body;
     dark_mode.style.backgroundColor = '#1d2a35';
@@ -28,8 +33,7 @@ const enableDarkMode = () => {
 };
 
 const disableDarkMode = () => {
-    
-    document.querySelector('#dark-mode').src="night-mode.png";
+    document.querySelector('#dark-mode').src="dark.png";
     // add dark-mode when it enable
     let dark_mode = document.body;
     dark_mode.style.backgroundColor = 'white';
@@ -37,13 +41,11 @@ const disableDarkMode = () => {
     side_nav.style.backgroundColor = 'white';
     let dark_nav = document.getElementById('nav');
     dark_nav.style.backgroundColor = '#c8e3fa';
-    
      // change text color
      const bright_text = document.querySelectorAll('.light');
      bright_text.forEach((light) => {
          light.style.color = 'black';
      });
-
     let dark_btn = document.getElementById('dark-btn');
     dark_btn.style.backgroundColor = '#c8e3fa';
     // set localstorge key to null 
@@ -61,17 +63,20 @@ DarkMode_toggle.addEventListener('click', () => {
     darkMode = localStorage.getItem('DarkMode');
     if (darkMode !== 'enable') {
         enableDarkMode();
+        
+        $(function(){
+            $('#dark-mode').animate({padding: '10px 20px 30px 50px'}).fadeIn(150);
+            $('#dark-mode').animate({padding: '0px'});
+            $('#dark-mode').addClass('rotation');
+            
+        });
 
     } else {
         disableDarkMode();
-
+        $('#dark-mode').animate({padding: '10px 20px 30px 50px'}).fadeIn(150);
+        $('#dark-mode').animate({padding: '0px'});
     };
 });
-
-
-
-
-
 
 
 /*
