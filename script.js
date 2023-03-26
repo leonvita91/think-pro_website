4610edba8ce98e95ba59dd76b8d9187c42e750c4
leonvita91 <leonvita91@gmail.com>
2023-03-26 13:09:33 +0100

    trying to optimaze js code

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 
 // read the key value from localstorge
+
 let darkMode = localStorage.getItem('DarkMode');
-
-
 const DarkMode_toggle = document.querySelector('#dark-mode');
 
-
-
 const enableDarkMode = () => {
-    
+    // orginize JS
     document.querySelector('#dark-mode').src="img/light.png";
-
+    //First bg color
+    const fst_dark_bg = document.querySelectorAll(".fst-dark-bg");
+    const sec_dark_bg = document.querySelectorAll(".sec-dark-bg");
+    const light_text = document.querySelectorAll(".light-text");
+    fst_dark_bg.forEach((dark_bg) => {
+        dark_bg.style.backgroundColor = '#335980';
+        sec_dark_bg.forEach((dark_bg) => {
+            dark_bg.style.backgroundColor = '#1d2a35';
+            light_text.forEach((light) => {
+                light.style.color = 'white';
+            });
+        });
+    });
     
-    // add dark-mode when it enable
+    localStorage.setItem('DarkMode','enable');
+    
+    //Second bg color
+        
+    /*
+    // Dark Navbar
     let dark_mode = document.body;
     dark_mode.style.backgroundColor = '#1d2a35';
     let side_nav = document.getElementById('side-nav-body');
@@ -20,18 +34,11 @@ const enableDarkMode = () => {
     let dark_nav = document.getElementById('nav');
     dark_nav.style.backgroundColor = '#335980';
     // change text color
-    const bright_text = document.querySelectorAll('.light');
-    bright_text.forEach((light) => {
-        light.style.color = 'white';
-    });
-
-    let dark_btn = document.getElementById('dark-btn');
-    dark_btn.style.backgroundColor = '#335980';
+    */
     // set localstorge key to enable
-    localStorage.setItem('DarkMode','enable');
 };
 
-const disableDarkMode = () => {
+const disableDarkMode = () => {    
     document.querySelector('#dark-mode').src="img/dark.png";
     // add dark-mode when it enable
     let dark_mode = document.body;
@@ -45,6 +52,8 @@ const disableDarkMode = () => {
      bright_text.forEach((light) => {
          light.style.color = 'black';
      });
+
+
     let dark_btn = document.getElementById('dark-btn');
     dark_btn.style.backgroundColor = '#c8e3fa';
     // set localstorge key to null 
