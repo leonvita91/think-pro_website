53c8839d5435d651394afb13e333a9111d6aff5f
leonvita91 <leonvita91@gmail.com>
2023-03-27 14:34:46 +0100

    add js objects

--- script.js
+++ script.js
@@ -4,55 +4,35 @@
 let darkMode = localStorage.getItem('DarkMode');
 const DarkMode_toggle = document.querySelector('#dark-mode');
 
+
 const enableDarkMode = () => {
     // orginize JS
     document.querySelector('#dark-mode').src="img/light.png";
-    //First bg color
-    const fst_dark_bg = document.querySelectorAll(".fst-dark-bg");
-    const sec_dark_bg = document.querySelectorAll(".sec-dark-bg");
-    const light_text = document.querySelectorAll(".light-text");
-    fst_dark_bg.forEach((dark_bg) => {
-        dark_bg.style.backgroundColor = '#335980';
-        sec_dark_bg.forEach((dark_bg) => {
-            dark_bg.style.backgroundColor = '#1d2a35';
-            light_text.forEach((light) => {
-                light.style.color = 'white';
-            });
-        });
-    });
-    
+    const en = {
+        dark : function (className,bg_color) {
+            this.className = className
+            this.bg_color = bg_color
+            const bg = document.querySelector(this.className)
+            bg.style.backgroundColor = this.bg_color
+        }
+    }
+
     localStorage.setItem('DarkMode','enable');
-    
-    //Second bg color
-        
-    /*
-    // Dark Navbar
-    let dark_mode = document.body;
-    dark_mode.style.backgroundColor = '#1d2a35';
-    let side_nav = document.getElementById('side-nav-body');
-    side_nav.style.backgroundColor = '#335980';
-    let dark_nav = document.getElementById('nav');
-    dark_nav.style.backgroundColor = '#335980';
-    // change text color
-    */
-    // set localstorge key to enable
+    new en.dark(".fst-dark-bg","green");
 };
 
-const disableDarkMode = () => {    
+const disableDarkMode = () => {
     document.querySelector('#dark-mode').src="img/dark.png";
-    // add dark-mode when it enable
-    let dark_mode = document.body;
-    dark_mode.style.backgroundColor = 'white';
-    let side_nav = document.getElementById('side-nav-body');
-    side_nav.style.backgroundColor = 'white';
-    let dark_nav = document.getElementById('nav');
-    dark_nav.style.backgroundColor = '#c8e3fa';
-     // change text color
-     const bright_text = document.querySelectorAll('.light');
-     bright_text.forEach((light) => {
-         light.style.color = 'black';
-     });
 
+    const dis = {
+        light : function (className,bg_color) {
+            this.className = className
+            this.bg_color = bg_color
+            const selector = document.querySelector(this.className)
+            selector.style.backgroundColor = this.bg_color
+        }
+    }
+    new dis.light('.fst-dark-bg','red')
 
     let dark_btn = document.getElementById('dark-btn');
     dark_btn.style.backgroundColor = '#c8e3fa';
