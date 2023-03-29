08310538739c7dc3fc47f64ef3e69e53b495da2a
leonvita91 <leonvita91@gmail.com>
2023-03-29 20:09:02 +0100

    new version of darkmode

--- script.js
+++ script.js
@@ -1,153 +1,29 @@
-// Create OOP 
 
-
-// Fucking confusing //
-
-class Dark_mode {
+class DarkMode {
     constructor() {
-        //initialize 
         this.status = localStorage.getItem('DarkMode');
-        // should call for if statment to checking which mode is on
-        if (this.status === 'Dark-Mode-Activeated'){
-            this.DarkMode_toggle = document.querySelector('#Switch-Mode');
-            this.DarkMode_toggle.addEventListener('click', () => {
-                this.enableDarkMode();
-                // add aninmation to Switching
-                $(function(){
-                    $('#Switch-Mode').animate({padding: '10px 20px 30px 50px'}).fadeIn(500);
-                    $('#Switch-Mode').animate({padding: '0px'});
-                    $('#Switch-Mode').addClass('rotation').fadeIn(1000);
-                });
-            });
-            // end method
-        } else if (this.status === 'Disable') {
-                this.Light_toggle = document.querySelector('#Switch-Mode');
-                this.Light_toggle.addEventListener('click', () => {
-                    this.disableDarkMode();
-            })
-        }
-            
+        this.enableDark()
+        
     };
+    
+    
+    enableDark()
+    {
+        let z = document.body
+        z.style.color = 'green'
+        // enable do something with desing
+        // this.choose = document.getElementsByClassName('fst-dark-bg')
+        // this.choose.style.color = 'green'
+        
+    }
 
-    enableDarkMode () {
-            document.querySelector('#Switch-Mode').src="img/light.png";
-            localStorage.setItem('DarkMode','Dark-Mode-Activeated');
-            console.log(this.status , `enable status`)
-            // add the customzation for colors
-
-                // animation on click will happen 
-                
-                
-                // } else {
-                //         disableDarkMode();
-                //         console.log('testing disable working')
-                //         $('#dark-mode').animate({padding: '10px 20px 30px 50px'}).fadeIn(1000);
-                //         $('#dark-mode').addClass('dark-rotation').fadeIn(500);
-                //         $('#dark-mode').animate({padding: '0px'});
-                //     };
-                // });
-
-            // if (this.status !== 'Dark-Mode-Activeated') {
-            //         // this.enableDarkMode();
-            // }
-        };          
-    disableDarkMode = () => {
-        document.querySelector('#Switch-Mode').src="img/dark.png";
-        localStorage.setItem('DarkMode','Disable');
-
-        console.log(this.status , `Disable status`)
+    active() {
+        this.dark_btn = document.querySelector('#Switch-Mode');
+        this.dark_btn.addEventListener('click', () => {
+    
+        });
+        
     }
 }
 
-obj = new Dark_mode()
-
-// enableDarkMode (class_name,bg_color){
-
-//     document.querySelector('#dark-mode').src="img/light.png";
-//     // Define objects 
-//     this.class_name = class_name
-//     this.bg_color = bg_color
-
-//     const dark_bg = document.querySelector(this.class_name)
-//     dark_bg.style.backgroundColor = this.bg_color
-
-//     localStorage.setItem('DarkMode','enable')
-// }
-
-// // tes = new Dark_mode('mewo');
-// // tes.dem()
-
-
-
-
-// // // Enable darkmode
-
-// // const enableDarkMode = () => {
-// //     document.querySelector('#dark-mode').src="img/light.png";
-// //     // object for class names,bg-color and text colors
-// //     const en = {
-// //         selector : localStorage.setItem('DarkMode','enable'),
-// //         dark_bg : function  (className,bg_color) {
-// //             this.className = className
-// //             this.bg_color = bg_color
-// //             const dark_bg = document.querySelector(this.className)
-// //             dark_bg.style.backgroundColor = this.bg_color
-            
-// //         },
-// //         dark_text : function  (class_text,color_text) {
-// //             this.class_text = class_text
-// //             this.color_text = color_text
-// //             const text = document.querySelector(this.class_text)
-// //             text.style.color = this.color_text
-// //         },
-// //     }
-
-//     //new en.dark_text('.logo-color' , 'white')
-
-//     // looping throgh class and colors
-//     let class_names = [
-//     '.fst-dark-bg',
-//     'body',
-//     '.side-dark-body'
-// ]
-//     let colors_names = [
-//     '#335980','#1d2a35','#335980'
-// ]
-//     for (let i = 0; i < 3; i++){
-//          new en.dark_bg(class_names[i],colors_names[i]);
-//     }
-// };
-
-// const disableDarkMode = () => {
-//     document.querySelector('#dark-mode').src="img/dark.png";
-//     const dis = {
-//         light : function (className,bg_color) {
-//             this.className = className
-//             this.bg_color = bg_color
-//             const selector = document.querySelector(this.className)
-//             selector.style.backgroundColor = this.bg_color
-//         }
-//     }
-
-//     // looping throgh class and colors
-//     let class_names = [
-//         '.fst-dark-bg',
-//         'body',
-//         '.side-dark-body',
-//     ]
-//         let colors_names = [
-//         '#ffffff','#ffffff','#ffffff'
-//     ]
-//         for (let i = 0; i < 3; i++){
-//             new dis.light(class_names[i],colors_names[i]);
-//         }
-//     localStorage.setItem('DarkMode',null);
-// };
-
-
-
-// // Jquery About us page
-// $(function() {
-//     $('#ayoub').hide(500).fadeOut(4000).fadeIn(4000);
-//     $('#ayoub').addClass('user-rotation');
-// });
+obj = new DarkMode()
