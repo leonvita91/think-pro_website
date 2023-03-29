46e4457ae118e56ca4db95f2f685f81870b6a8be
leonvita91 <leonvita91@gmail.com>
2023-03-29 22:49:57 +0100

    rebuild darkmode

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
-
 class DarkMode {
     constructor() {
-        this.status = localStorage.getItem('DarkMode');
-        this.enableDark()
-        
+        this.status = localStorage.getItem('enable');
+        console.log(this.status)
+        this.active()
     };
-    
-    
-    enableDark()
-    {
-        let z = document.body
-        z.style.color = 'green'
+
+    enableDark(class_name,bg_color) {
         // enable do something with desing
-        // this.choose = document.getElementsByClassName('fst-dark-bg')
-        // this.choose.style.color = 'green'
-        
+        this.class_name = class_name
+        this.bg_color = bg_color
+        this.icon = document.querySelector('#Switch-Mode').src = 'img/light.png'
+        this.css_class = document.querySelector(class_name);
+        this.css_class.style.backgroundColor = bg_color;
+        this.status = localStorage.setItem('enable','DarkMode');
+    }
+    disableDark() {
+        this.choose = document.querySelector('.fst-dark-bg');
+        this.choose.style.backgroundColor = 'red';
+        this.status = localStorage.setItem('enable','LightMode');
     }
 
     active() {
         this.dark_btn = document.querySelector('#Switch-Mode');
         this.dark_btn.addEventListener('click', () => {
-    
-        });
-        
-    }
+        this.status = localStorage.getItem('enable');
+        console.log(this.status)
+            if (this.status !== 'DarkMode') {
+                this.enableDark('.fst-dark-bg','green')
+            } else {
+                this.disableDark()
+            }
+        });        
+    }   
 }
-
 obj = new DarkMode()
