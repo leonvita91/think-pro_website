3c50a865adea5e931902ead7a294317ffe905881
leonvita91 <leonvita91@gmail.com>
2023-04-01 22:26:05 +0100

    latest update

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ class DarkMode {
             this.enableDark()
         } else {
             this.disableDark()
+            $('#Switch-Mode').addClass('rotations');
         }
         // Callback method
         this.active()
@@ -16,38 +17,42 @@ class DarkMode {
         // set dark-mode option 
         this.status = localStorage.setItem('enable','DarkMode');
         // switch to dark-logo
-        this.icon = document.querySelector('#Switch-Mode').src="img/dark.png";
+        this.icon = document.querySelector('#Switch-Mode').src="icons/home/dark.png";
         // Define the Classes_background and Colors
         this.classList = ['.Dark-nav','.darkmode-body','.side-dark-body']
-        this.colorList = ['green','yellow','blue']
+        this.colorList = ['#1d2a35','#0d1721','#15202b']
         //  Apply Looping
         for (let i =0; i < this.classList.length;i++) {
             this.selector_class = document.querySelector(this.classList[i]);
             this.selector_class.style.backgroundColor = this.colorList[i];
         }
         // Define the Classes_Text and Colors
-        this.classList = ['.logo-color','.side-nav-text']
-        this.colorList = ['white','white']
+        this.classList = ['.logo-color','.side-nav-home','.side-nav-about-us']
+        this.colorList = ['white','white','white']
         //  Apply Looping
         for (let i =0; i < this.classList.length;i++) {
             this.selector_class = document.querySelector(this.classList[i]);
             this.selector_class.style.color = this.colorList[i];
         }
+        $(document).ready(function() {
+            $('#Switch-Mode').fadeIn(1500);
+
+        })
     }
     disableDark() {
-        this.icon = document.querySelector('#Switch-Mode').src="img/light.png";
+        this.icon = document.querySelector('#Switch-Mode').src="icons/home/light.png";
         this.status = localStorage.setItem('enable','LightMode');
         // Define the Classes_background and Colors
         this.classList = ['.Dark-nav','.darkmode-body','.side-dark-body']
-        this.colorList = ['grey','white','red']
+        this.colorList = ['#5800FF','#0096FF','#00D7FF']
         //  Apply Looping
         for (let i =0; i < this.classList.length;i++) {
             this.selector_class = document.querySelector(this.classList[i]);
             this.selector_class.style.backgroundColor = this.colorList[i];
         }
         // Define the Classes_Text and Colors
-        this.classList = ['.logo-color','.side-nav-text']
-        this.colorList = ['black','black']
+        this.classList = ['.logo-color']
+        this.colorList = ['white']
         //  Apply Looping
         for (let i =0; i < this.classList.length;i++) {
             this.selector_class = document.querySelector(this.classList[i]);
@@ -60,8 +65,15 @@ class DarkMode {
         this.status = localStorage.getItem('enable');
             if (this.status !== 'DarkMode') {
                 this.enableDark()
+                $('#Switch-Mode').animate({padding: '10px 20px 30px 50px'}).fadeIn(500);
+                $('#Switch-Mode').animate({padding: '0px'});
+                $('#Switch-Mode').addClass('rotations');
+                    
             } else {
                 this.disableDark()
+                $('#Switch-Mode').animate({padding: '10px 20px 30px 50px'}).fadeIn(1000);
+                $('#Switch-Mode').animate({padding: '0px'});
+                $('#Switch-Mode').addClass('rotations');
             }
         });
     }
