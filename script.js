5fa776c41af95085d935b024e44de54c5617d37e
leonvita91 <leonvita91@gmail.com>
2023-04-06 14:58:56 +0100

    last update to the site

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Dark Mode Class
 class DarkMode {
     constructor() {
         // initialize
@@ -13,6 +14,8 @@ class DarkMode {
         // Callback method
         this.active()
     };
+
+    // Enable Dark-mode
     enableDark() {
         // set dark-mode option 
         this.status = localStorage.setItem('enable','DarkMode');
@@ -24,51 +27,83 @@ class DarkMode {
         '.darkmode-body',
         '.side-dark-body',
         '.para-container h1',
-        '.para-container p']
+        '.para-container p'
+    ]
         this.colorList = [
-        '#1d2a35',
-        '#0d1721',
-        '#15202b',
-        '#1d2a35',
-        '#1d2a35']
+        '#1A374D',
+        '#406882',
+        '#6998AB',
+        '#6998AB',
+        '#1A374D',
+    ]
         //  Apply Looping
         for (let i =0; i < this.classList.length;i++) {
             this.selector_class = document.querySelector(this.classList[i]);
             this.selector_class.style.backgroundColor = this.colorList[i];
         }
         // Define the Classes_Text and Colors
-        this.classList = ['.logo-color','.side-nav-home','.side-nav-about-us']
-        this.colorList = ['white','white','white']
+        this.classList = [
+            '.logo-color',
+            '.side-nav-home',
+            '.side-nav-about-us',
+            '.para-container h1',
+            '.para-container p'
+        ]
+        this.colorList = [
+            'white',
+            'white',
+            'white',
+            'white',
+            'white'
+        ]
         //  Apply Looping
         for (let i =0; i < this.classList.length;i++) {
             this.selector_class = document.querySelector(this.classList[i]);
             this.selector_class.style.color = this.colorList[i];
         }
-        $(document).ready(function() {
-            $('#Switch-Mode').fadeIn(1500);
-
-        })
     }
+    // Disable Dark-mode
     disableDark() {
         this.icon = document.querySelector('#Switch-Mode').src="icons/home/dark.png";
         this.status = localStorage.setItem('enable','LightMode');
         // Define the Classes_background and Colors
-        this.classList = ['.Dark-nav','.darkmode-body','.side-dark-body','.para-container h1']
-        this.colorList = ['#5800FF','#0096FF','#00D7FF','green']
+        this.classList = [
+            '.Dark-nav',
+            '.darkmode-body',
+            '.side-dark-body',
+            '.para-container h1',
+            '.para-container p'
+        ]
+        this.colorList = [
+            '#C3F8FF',
+            '#ABD9FF',
+            '#ABD9FF',
+            '#ABD9FF',
+            '#ABD9FF'
+        ]
         //  Apply Looping
         for (let i =0; i < this.classList.length;i++) {
             this.selector_class = document.querySelector(this.classList[i]);
             this.selector_class.style.backgroundColor = this.colorList[i];
         }
         // Define the Classes_Text and Colors
-        this.classList = ['.logo-color']
-        this.colorList = ['white']
+        this.classList = [
+            '.logo-color',
+            '.para-container h1',
+            '.para-container p'
+        ]
+        this.colorList = [
+            'black',
+            'black',
+            'black'
+        ]
         //  Apply Looping
         for (let i =0; i < this.classList.length;i++) {
             this.selector_class = document.querySelector(this.classList[i]);
             this.selector_class.style.color = this.colorList[i];
         }
     }
+    // on click method
     active() {
         this.dark_btn = document.querySelector('#Switch-Mode');
         this.dark_btn.addEventListener('click', () => {
