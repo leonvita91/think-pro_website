74ba0ad663893005e62ebc35d05f3783724f2b56
leonvita91 <leonvita91@gmail.com>
2023-04-11 16:54:53 +0100

    login fixed page

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from website import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=3000, debug=True)
+    app.run(host='10.10.11.2', port=3000, debug=True)
